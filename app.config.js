8162dc8c1db46e68f3c019e8083801c1350c8d78
hansuribe <hansuribe@gmail.com>
2020-10-05 20:38:09 -0500

    Initial commit

---
+++ app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Svelte, Routify, MDsveX',
+  url: 'https://svelte-routify-mdsvex-demo.vercel.app',
+  description: 'Demonstration for Svelte, Routify, and MDsveX',
+  keywords: ['svelte', 'routify', 'mdsvex', 'mdx', 'demo'],
+  author: 'josefaidt',
+}
